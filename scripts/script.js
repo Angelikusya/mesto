8f235f93fc23b6316501cb2053b45ee4a81e0c91
Anzhelika <angelikachashchina@gmail.com>
2023-05-26 19:10:48 +0300

    after 1 review

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+//поставить лайк
+
+let vectors = document.querySelectorAll('.element__vector'); // кнопка с лайками 
+
+ 
+
+//функция, если нет лайка, то поставить лайк. Если лайк есть, то убрать его 
+
+vectors.forEach(function(vector) { 
+  vector.addEventListener('click', function() { 
+        vector.classList.add('element__vector_active'); 
+        vector.classList.remove('button');
+}) }); 
+
+
+// РАБОТА С ПОПАПОМ
+
+//вызов селекторов для работы с редактирумой формой 
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let buttonClose = document.querySelector('.popup__button-close');
+
+//открытие попапа по клику на кнопку "редактировать"
+
+//открытие попапа по клику на кнопку "редактировать"
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+function handleFormSubmit(evt) {
+  evt.preventDefault(); // Отменить стандартное поведение формы
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
+}
+
+//закрытие попапа на кнопку крестик
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+// слушатели функций
+editButton.addEventListener('click', openPopup);
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+buttonClose.addEventListener('click', closePopup);
